34f888c04eef594cafabc060b91ef82564311a79
Your Name <nusret-24@hotmail.co.uk>
2021-03-31 15:31:24 +0200

    some more changes to editor buttons and callback for save

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Dropdown, SplitButton } from 'react-bootstrap';
+import {Dropdown, ButtonGroup, Button } from 'react-bootstrap';
 import Editor from './Components/SunEditor';
 
 
 function App() {
   return (
     <div className="App">
-      <SplitButton
-        key={1}
-        id={`dropdown-split`}
-        variant='Success'
-        title='test'
-      >
-        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-        <Dropdown.Item eventKey="3" active>
-          Active Item
-        </Dropdown.Item>
-        <Dropdown.Divider />
-        <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-      </SplitButton>
+      <Dropdown as={ButtonGroup}>
+  <Button variant="success">TV</Button>
+
+  <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Intern</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Extern</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
       <Editor />
     </div>
   );
--- src/Components/SunEditor.js
+++ src/Components/SunEditor.js
@@ -4,39 +4,62 @@ import "suneditor/dist/css/suneditor.min.css";
 
 /* Användning för att justera toolbar, vad som skall finnas med*/
 const Settings = {
-  imageFileInput: false,
-  buttonList: [
-    ["codeView", "undo", "redo", "font", "fontSize", "formatBlock"],
-    [
-      "paragraphStyle",
-      "blockquote",
-      "bold",
-      "underline",
-      "italic",
-      "strike",
-      "subscript",
-      "superscript"
+	"mode": "classic",
+	"rtl": false,
+	"katex": "window.katex",
+	"imageGalleryUrl": "https://etyswjpn79.execute-api.ap-northeast-1.amazonaws.com/suneditor-demo",
+	"videoFileInput": false,
+	"tabDisable": false,
+	"buttonList": [
+		[
+			"undo",
+			"redo",
+			"font",
+			"fontSize",
+			"formatBlock"
     ],
-    '/',
-    ["fontColor", "hiliteColor", "textStyle", "removeFormat"],
     [
-      "outdent",
-      "indent",
-      "align",
-      "horizontalRule",
-      "list",
-      "lineHeight",
-      "table"] ,
+			"paragraphStyle",
+			"blockquote",
+			"bold",
+			"underline",
+			"italic",
+			"strike",
+			"subscript",
+			"superscript",
+			"fontColor",
+			"hiliteColor",
+			"textStyle",
+			"removeFormat"],
+      "/",
       [
-      "link",
-      "image",
-      "video",
-      "fullScreen",
-      "showBlocks",
-      "print",
-      "save"
-    ]
-  ]
+			"outdent",
+			"indent",
+			"align",
+			"horizontalRule",
+			"list",
+			"lineHeight"],
+      [
+			"table",
+			"link",
+			"image",
+			"video",
+			"audio",
+			"imageGallery"],
+      [
+			"fullScreen",
+			"showBlocks",
+			"codeView",
+			"preview"],
+      [
+			"print",
+			"save",
+			"template"
+		]
+	],
+  callBackSave: function (contents, isChanged) {
+    alert(contents);
+}
 };
 
 export default function Editor() {
